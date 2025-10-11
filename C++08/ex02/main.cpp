da964a19543e4d00332712adca1b038169054a2b
Sertan Göktuğ Başarıcı <sebasari@k1m07s09.42istanbul.com.tr>
2025-10-11 21:02:08 +0300

    C++8

--- C++08/ex02/main.cpp
+++ C++08/ex02/main.cpp
@@ -5,7 +5,6 @@ int main()
 {
     MutantStack<int> mstack;
 
-    // normal stack gibi kullan
     mstack.push(5);
     mstack.push(17);
     std::cout << "top: " << mstack.top() << std::endl;
@@ -17,8 +16,6 @@ int main()
     mstack.push(5);
     mstack.push(737);
     mstack.push(0);
-
-    // iterator kullanarak gezin
     MutantStack<int>::iterator it = mstack.begin();
     MutantStack<int>::iterator ite = mstack.end();
 
